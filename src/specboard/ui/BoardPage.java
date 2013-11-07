fcc01488e1642d63e551aef7c933823fd34f2614
Jaakko Rinta-Filppula <jaakko.rf@gmail.com>
2013-11-08 00:05:16 +0200

    methods for changing grid size

--- src/specboard/ui/BoardPage.java
+++ src/specboard/ui/BoardPage.java
@@ -4,6 +4,7 @@ import javafx.scene.control.Tab;
 import javafx.scene.layout.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,7 +16,7 @@ public class BoardPage extends Tab {
     private String name;
     private GridPane grid;
 
-    private List<SoundCell> cells;
+    private SoundCell[][] cells;
 
     public BoardPage(String name, int rows, int cols) {
         super(name);
@@ -26,32 +27,76 @@ public class BoardPage extends Tab {
         this.name = name;
 
         //create grid
-        this.grid = new GridPane();
+        grid = new GridPane();
 
-        this.cells = new ArrayList<>();
+        cells = new SoundCell[rows][cols];
+
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                SoundCell newCell = new SoundCell("");
+                cells[r][c] = new SoundCell("");
+            }
+        }
 
-                this.cells.add(newCell);
+        grid = createGrid(cells);
 
-                this.grid.add(newCell, c, r);
+        setContent(grid);
+    }
+
+    public void setGridSize(int rows, int cols) {
+        if (rows == this.rows && cols == this.cols) {
+            return;
+        }
+
+        SoundCell[][] newCells = new SoundCell[rows][cols];
+
+        for (int r = 0; r < Math.min(rows, this.rows); r++) {
+            newCells[r] = Arrays.copyOf(cells[r], cols);
+        }
+
+        //Fill out added cells
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (newCells[r][c] == null) {
+                    newCells[r][c] = new SoundCell("");
+                }
+            }
+        }
+
+        grid = createGrid(newCells);
+        cells = newCells;
+        this.rows = rows;
+        this.cols = cols;
+
+        setContent(grid);
+    }
+
+
+    private GridPane createGrid(SoundCell[][] cells) {
+        GridPane result = new GridPane();
+
+        int rows = cells.length,
+            cols = cells[0].length;
+
+        for (int r = 0; r < cells.length; r++) {
+            for (int c = 0; c < cells[r].length; c++) {
+                result.add(cells[r][c], c, r);
             }
         }
 
         for (int r = 0; r < rows; r++) {
             RowConstraints row = new RowConstraints(100, 100, Double.MAX_VALUE);
             row.setVgrow(Priority.ALWAYS);
-            grid.getRowConstraints().add(row);
+            result.getRowConstraints().add(row);
         }
 
         for (int c = 0; c < cols; c++) {
             ColumnConstraints col = new ColumnConstraints(100, 100, Double.MAX_VALUE);
             col.setHgrow(Priority.ALWAYS);
-            grid.getColumnConstraints().add(col);
+            result.getColumnConstraints().add(col);
         }
 
-        grid.setGridLinesVisible(true);
-        this.setContent(grid);
+        result.setGridLinesVisible(true);
+
+        return result;
     }
 }
